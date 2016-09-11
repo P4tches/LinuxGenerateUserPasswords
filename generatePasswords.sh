61db09c5947d893a873d22728e5b503243d6cfd4
P4tches <P4tches@users.noreply.github.com>
2016-09-11 16:45:58 -0700

    Removed unnecessary lines of code

--- generatePasswords.sh
+++ generatePasswords.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-space=$(echo "     ")
-c=$(echo ":")
 for i in $(getent passwd | grep /home/ | cut -d ':' -f 1) ; do
-PWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-echo $i$space$PWD >> ./passwords.txt
+PWD=$(cat /dev/urandom | tr -dc 'a-zA-Z1-9' | fold -w 8 | head -n 1)
+echo $i"    "$PWD >> ./passwords.txt
 chpasswd <<<"$i:$PWD"
 done
