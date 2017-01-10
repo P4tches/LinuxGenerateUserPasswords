70300cf24b76e440c383b097759847f84bbb33f3
frag <fragtastic@users.noreply.github.com>
2017-01-09 22:02:38 -0800

    Don't delete password.txt on openssl failure

--- generatePasswords.sh
+++ generatePasswords.sh
@@ -5,5 +5,4 @@ for i in $(sudo awk '!/*|!/' /etc/shadow | cut -d ':' -f 1) ; do
 	echo $i"    "$PWD >> passwords.txt
 	chpasswd <<< i "$i:$PWD"
 done
-openssl des3 -salt -in passwords.txt -out passwords.enc
-rm passwords.txt
+openssl des3 -salt -in passwords.txt -out passwords.enc && rm passwords.txt
