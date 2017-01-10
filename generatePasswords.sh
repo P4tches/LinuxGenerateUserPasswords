a5b99ba10878b62c192301c13244cd1a3aa5783f
frag <fragtastic@users.noreply.github.com>
2017-01-09 21:58:38 -0800

    Updated script
    Using pwgen for convenience.
    Select only users with passwords instead of if there is a home directory or not.
    Added encryption of the password file.

--- generatePasswords.sh
+++ generatePasswords.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-for i in $(getent passwd | grep /home/ | cut -d ':' -f 1) ; do
-PWD=$(cat /dev/urandom | tr -dc 'a-zA-Z1-9' | fold -w 8 | head -n 1)
-echo $i"    "$PWD >> ./passwords.txt
-chpasswd <<<"$i:$PWD"
+for i in $(sudo awk '!/*|!/' /etc/shadow | cut -d ':' -f 1) ; do
+	PWD=$(pwgen -1s 15 1)
+	echo $i"    "$PWD >> passwords.txt
+	chpasswd <<< i "$i:$PWD"
 done
+openssl des3 -salt -in passwords.txt -out passwords.enc
+rm passwords.txt
