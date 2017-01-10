d3b4bcb031d5079dab84bcf685099c84e57baa97
frag <fragtastic@users.noreply.github.com>
2017-01-09 22:09:11 -0800

    Removed extraneous "i"

--- generatePasswords.sh
+++ generatePasswords.sh
@@ -3,6 +3,6 @@ cd "$(dirname "$0")"
 for i in $(awk '!/*|!/' /etc/shadow | cut -d ':' -f 1) ; do
 	PWD=$(pwgen -1s 15 1)
 	echo $i"    "$PWD >> passwords.txt
-	chpasswd <<< i "$i:$PWD"
+	chpasswd <<< "$i:$PWD"
 done
 openssl des3 -salt -in passwords.txt -out passwords.enc && rm passwords.txt
