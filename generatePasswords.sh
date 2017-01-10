a711056503b2a2cc758dba666a47e23774df6cd9
frag <fragtastic@users.noreply.github.com>
2017-01-09 22:03:19 -0800

    Removed sudo

--- generatePasswords.sh
+++ generatePasswords.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-for i in $(sudo awk '!/*|!/' /etc/shadow | cut -d ':' -f 1) ; do
+for i in $(awk '!/*|!/' /etc/shadow | cut -d ':' -f 1) ; do
 	PWD=$(pwgen -1s 15 1)
 	echo $i"    "$PWD >> passwords.txt
 	chpasswd <<< i "$i:$PWD"
